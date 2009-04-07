cf103866efbd94f83f5238891f946b2f618889ed
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2009-04-07 16:51:38 +0200

    Add shoulda macros

---
+++ shoulda_macros/has_remote_macros.rb
@@ -0,0 +1,22 @@
+module HasRemoteMacros
+  
+  def should_have_remote(&block)
+    klass = model_class
+    should "have remote resources" do
+      assert HasRemote.models.include?(klass)
+      assert klass.const_defined?("Remote")
+    end
+  end
+  
+  def should_have_remote_attribute(attr_name, options = {})
+    klass = model_class
+    should "have remote attribute #{attr_name}" do
+      assert klass.remote_attributes.include?(attr_name)
+      assert klass.cached_attributes.include?(attr_name) if options[:local_cache]
+      assert klass.new.respond_to?(options[:as] || attr_name)
+    end
+  end
+  
+end
+
+Test::Unit::TestCase.extend HasRemoteMacros
