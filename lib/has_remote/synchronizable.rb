29427380817229550e7691405016a48daf70e8e2
Sam Aaron <samaaron@gmail.com>
2010-01-22 16:58:04 +0100

    add support for limits

--- lib/has_remote/synchronizable.rb
+++ lib/has_remote/synchronizable.rb
@@ -1,5 +1,5 @@
 module HasRemote
-  
+
   # Contains class methods regarding synchronization of changed, added and deleted remotes.
   #
   # === Synchronization examples
@@ -20,8 +20,8 @@ module HasRemote
   # your cache is up to date or use the 'hr:sync' rake task to synchronize all models
   # from the command line.
   #
-  # *Note* 
-  # All remote resources need to have an 'updated_at' field in order for synchronization to work. Records will 
+  # *Note*
+  # All remote resources need to have an 'updated_at' field in order for synchronization to work. Records will
   # be destroyed if their remote resource's 'deleted_at' time lies before the time of synchronization.
   #
   module Synchronizable
@@ -31,7 +31,7 @@ module HasRemote
     def cached_attributes
       @cached_attributes ||= []
     end
-    
+
     # Returns all remote objects that have been changed since the given time or one week ago if no
     # time is given. This may include new and optionally deleted (tagged by a 'deleted_at' attribute) resources.
     #
@@ -47,11 +47,14 @@ module HasRemote
     #    User::Remote.find :all, :from => :search, :params => {:updated_since => time.strftime('...') }
     #  end
     #
-    def changed_remotes_since(time = nil)
-      time ||= 1.week.ago 
-      remote_class.find :all, :from => :updated, :params => {:since => time.to_s}  
+    # @param [DateTime] time The given time that defines since (see above)
+    # @param [Hash] params Additional params to send with the request (i.e. :limit)
+    #
+    def changed_remotes_since(time = nil, params = {})
+      time ||= 1.week.ago
+      remote_class.find :all, :from => :updated, :params => {:since => time.to_s}.merge(params)
     end
-    
+
     # Will update all records that have been created, updated or deleted on the remote host
     # since the last successful synchronization.
     #
@@ -59,7 +62,7 @@ module HasRemote
       logger.info( "*** Start synchronizing #{table_name} at #{Time.now.to_s :long} ***\n" )
       @sync_count = 0
       begin
-        changed_objects = changed_remotes_since( options[:since] || synchronized_at )
+        changed_objects = changed_remotes_since( options.delete(:since) || synchronized_at, options )
         if changed_objects.any?
           # Do everything within transaction to prevent ending up in half-synchronized situation if an exception is raised.
           transaction { sync_all_records_for(changed_objects) }
@@ -69,19 +72,19 @@ module HasRemote
       rescue => e
         logger.warn( " - Synchronization of #{table_name} failed: #{e} \n #{e.backtrace}" )
       else
-        self.synchronized_at = changed_objects.map { |o| time_of_update(o) }.sort.last if changed_objects.any?  
+        self.synchronized_at = changed_objects.map { |o| time_of_update(o) }.sort.last if changed_objects.any?
         logger.info( " - Synchronized #{@sync_count} #{table_name}.\n" ) if @sync_count > 0
       ensure
         logger.info( "*** Stopped synchronizing #{table_name} at #{Time.now.to_s :long} ***\n" )
       end
     end
-    
+
     # Time of the last successful synchronization.
     #
     def synchronized_at
       HasRemote::Synchronization.for(self.name).latest_change
     end
-    
+
   private
 
     def synchronized_at=(time) #:nodoc:
@@ -91,7 +94,7 @@ module HasRemote
     def sync_all_records_for(resources) #:nodoc:
       resources.each { |resource| sync_all_records_for_resource(resource) }
     end
-    
+
     def sync_all_records_for_resource(resource) #:nodoc:
       records = find(:all, :conditions => ["#{remote_foreign_key} = ?", resource.send(remote_primary_key)])
       if records.empty?
@@ -100,7 +103,7 @@ module HasRemote
         records.each { |record| sync_record_for_resource(record, resource) }
       end
     end
-    
+
     def sync_record_for_resource(record, resource) #:nodoc:
       if deleted?(resource)
         delete_record_for_resource(record, resource)
@@ -108,7 +111,7 @@ module HasRemote
         update_and_save_record_for_resource(record, resource)
       end
     end
-    
+
     def update_and_save_record_for_resource(record, resource) #:nodoc:
       was_it_new = record.new_record?
       cached_attributes.each do |remote_attr|
@@ -121,24 +124,24 @@ module HasRemote
         logger.info( was_it_new ? " - Created #{name.downcase} with id #{record.id}.\n" : " - Updated #{name.downcase} with id #{record.id}.\n" )
       end
     end
-    
+
     def delete_record_for_resource(record, resource) #:nodoc:
       record.destroy
       @sync_count += 1
       logger.info( " - Deleted #{name.downcase} with id #{record.id}.\n" )
     end
-    
+
     def create_record_for_resource(resource) #:nodoc:
       update_and_save_record_for_resource(new(remote_foreign_key => resource.send(remote_primary_key)), resource)
     end
-    
+
     def time_of_update(resource)
       (resource.respond_to?(:deleted_at) && resource.deleted_at) ? resource.deleted_at : resource.updated_at
     end
-    
+
     def deleted?(resource)
       resource.respond_to?(:deleted_at) && resource.deleted_at && resource.deleted_at <= Time.now
     end
 
-  end  
+  end
 end
--- lib/tasks/has_remote.rake
+++ lib/tasks/has_remote.rake
@@ -3,7 +3,11 @@ namespace :hr do
   desc 'Synchronizes all attributes locally cached by has_remote'
   task :sync => :environment do
     models = ENV['MODELS'].nil? ? HasRemote.models : extract_models
-    models.each(&:synchronize!)
+    options = {}
+    options[:limit] = ENV['LIMIT'] if ENV['LIMIT']
+    options[:since] = ENV['SINCE'] if ENV['SINCE']
+
+    models.each{|model| model.synchronize!(options)}
   end
 
   def extract_models
