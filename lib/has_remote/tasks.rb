912b0520b9fed5516685a83b1677385a8232e968
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2010-02-18 14:33:14 +0100

    update hr:sync rake task

--- lib/has_remote/tasks.rb
+++ lib/has_remote/tasks.rb
@@ -2,7 +2,7 @@ namespace :hr do
 
   desc 'Synchronizes all attributes locally cached by has_remote'
   task :sync => :environment do
-    models = ENV['MODELS'].nil? ? HasRemote.models : extract_models
+    models = ENV['MODELS'] ? extract_models : HasRemote.models
     options = ENV['PARAMS'] ? Rack::Utils.parse_query(ENV['PARAMS']) : {}
     models.each{|model| model.synchronize!(options)}
   end
