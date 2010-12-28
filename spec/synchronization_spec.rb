20514ce264b3355c39c3892f93ffc9ba22eb6b57
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2010-12-28 13:04:41 +0100

    Replace usage of have_named_sope matcher which was removed from Shoulda.

--- spec/synchronization_spec.rb
+++ spec/synchronization_spec.rb
@@ -7,6 +7,16 @@ describe HasRemote::Synchronization do
   it { should validate_presence_of(:model_name) }
   it { should validate_presence_of(:last_record_updated_at) }
   it { should validate_presence_of(:last_record_id) }
-  it { should have_named_scope("for('User')") }
+
+  describe "named scope 'for'" do
+    before do
+      @user_synchronization = HasRemote::Synchronization.create! :model_name => 'User', :last_record_updated_at => 1.day.ago, :last_record_id => 1
+      @book_synchronization = HasRemote::Synchronization.create! :model_name => 'Book', :last_record_updated_at => 1.day.ago, :last_record_id => 2
+    end
+    
+    it "should return synchronization records scoped by model_name" do
+      HasRemote::Synchronization.for('User').should == [@user_synchronization]
+    end
+  end
 
 end
